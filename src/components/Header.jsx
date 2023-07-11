5b0b006a1801520ba373e38a15590ec51fc92f0b
rusoalcaraz <manuel_90_1@hotmail.com>
2023-07-10 20:10:05 -0600

    No fue mucho trabajo pero fue honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <>
       <>
         <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
-          Seguimiento Pacientes{" "}
+          Seguimiento Pacientes con Github{" "}
           <span className="text-azulSwear-100">Veterinaria</span>
         </h1>
       </>
